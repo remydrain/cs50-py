c82eb6784d2bc4cef83dd1b7a27e8e2800a1f02a
Remy Drain <remydrain@gmail.com>
2023-12-06 15:11:18 -0500

    Add calculator.py

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
-# ask user for their name
-name = input("What's your name? ")
+# ask user for their name and format for whitespace and capitalization
+name = input("What's your name? ").strip().title()
+
+# split user's name into first and last
+first, last = name.split(" ")
 
 # say hello to user
-print(f"Hello, {name}")
+print(f"Hello, {first}")
